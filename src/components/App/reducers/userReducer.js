e9b95cafbadb63ba41515acb031ef8333f1e6af9
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-08-10 15:47:25 -0700

    adds store setup, prover to index.js, begins reducer with intial state, and adds a selector to select a list of users

---
+++ src/components/App/reducers/userReducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+  users: []
+};
+
+export default function reducer(state, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/components/selectors/usersSelectors.test.js
@@ -0,0 +1,31 @@
+const { getUsers } = require('./usersSelectors');
+
+describe('users selectors', () => {
+  it('selects a list of users from state', () => {
+    const state = {
+      users: [
+        {
+          username: 'testUser1',
+          profileImage: 'myimage.png'
+        },
+        {
+          username: 'testUser2',
+          profileImage: 'myimage2.png'
+        }
+      ]
+    };
+
+    const users = getUsers(state);
+
+    expect(users).toEqual([
+      {
+        username: 'testUser1',
+        profileImage: 'myimage.png'
+      },
+      {
+        username: 'testUser2',
+        profileImage: 'myimage2.png'
+      }
+    ]);
+  });
+});
